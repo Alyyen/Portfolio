f8e2becd6a08ef5cb420df66f5a172ad3562be46
Alyyen <alyssia.colomar@gmail.com>
2022-07-13 15:52:40 +0200

    Add favicon + back to top button

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(function () {
+    let $mybutton = document.getElementById("myBtn");
 
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            $mybutton.style.display = "block";
+        } else {
+            $mybutton.style.display = "none";
+        }
+    }
 });
